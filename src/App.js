86f9c49a05522a46d2b7197d93310c145a5db7e5
reloadercf <elcfm1@gmail.com>
2019-06-29 17:01:55 -0500

    Utilizando state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React,{Fragment} from 'react';
 //fragment nos sirve para no usar los divs esto nos puede ayudar a segmentar codigo en css
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import Productos from './components/Productos'
 function App() {
   let fecha =new Date().getFullYear()
   return (
     <Fragment>
       <Header titulo='tieda virtual' />
+      <Productos/>
       <Footer fecha={fecha}/>
     </Fragment>
   );
