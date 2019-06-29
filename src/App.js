fe528fd0504d702272b7250433c356643b81c696
reloadercf <elcfm1@gmail.com>
2019-06-29 16:48:26 -0500

    usando Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React,{Fragment} from 'react';
 //fragment nos sirve para no usar los divs esto nos puede ayudar a segmentar codigo en css
-import PrimerComponente from './components/PrimerComponente'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
+  let fecha =new Date().getFullYear()
   return (
     <Fragment>
-      <PrimerComponente/>
-      <PrimerComponente/>
-      <PrimerComponente/>
+      <Header titulo='tieda virtual' />
+      <Footer fecha={fecha}/>
     </Fragment>
   );
 }
